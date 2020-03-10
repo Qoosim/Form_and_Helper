bd6357f63939058df4a48c8cd738a96888733f6b
Rory Hellier <rory.hellier@googlemail.com>
2020-03-10 16:29:09 +0000

    Added editing action for the users.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,11 +3,24 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
   def create
     @user = User.new(user_params)
 
     if @user.save
-      redirect_to users_url
+      redirect_to @user
     else
       render 'new'
     end
