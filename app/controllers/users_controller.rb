06da4a1e42ae8ae5dd6cfbf1d6fc93535c5ebf4a
Rory Hellier <rory.hellier@googlemail.com>
2020-03-10 11:48:08 +0000

    Added strong params, added create option for new users and routed the form to submit new users into the database

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,18 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to users_url
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password, :password_confirmation)
   end
 end
